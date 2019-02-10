0f3e539cb4ff415527064567e9a3fc6863742f58
Leo Laksmana <beol@laksmana.com>
2019-02-11 07:02:00 +0800

    Break up components

---
+++ src/js/components/header-nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const HeaderNav = () => (
+  <header>
+    <nav>
+      <a href="#" className="brand">Foo</a>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Documentation</a></li>
+        <li><a href="#">API Reference</a></li>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default HeaderNav;
--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,78 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import HeaderNav from './components/header-nav';
+import Sidebar from './components/sidebar';
+import Footer from './components/footer';
+
 import '../scss/main.scss';
 
 const App = () => (
-  <article>
-    <header>
-      <h1>Lorem Ipsum</h1>
-    </header>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque finibus convallis diam in tristique. Nunc varius erat a urna fermentum blandit. Sed nec facilisis diam. Pellentesque ut nibh mattis, eleifend nulla a, mollis magna. Aliquam non condimentum urna. Vestibulum tempus massa nec leo consequat, nec hendrerit arcu convallis. In hac habitasse platea dictumst. Suspendisse potenti. Phasellus dictum pellentesque posuere. Nunc placerat augue ut iaculis ultricies. Nam non egestas enim. Suspendisse potenti.</p>
+  <div className="page-grid-container">
+    <HeaderNav />
+    <Sidebar />
+    <main role="main">
+      <article>
+        <header>
+          <h1>Lorem Ipsum</h1>
+        </header>
 
-    <footer>
-      <small>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</small>
-    </footer>
-  </article>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque convallis fringilla mi, quis mattis nisi facilisis vehicula. In vel tempus magna. Quisque vitae velit tellus. Fusce commodo urna sit amet lectus eleifend, sit amet pharetra tortor fermentum. Nullam fringilla turpis ipsum, ac pellentesque mi rutrum vitae. Etiam aliquet elit eu nunc mollis ullamcorper. Phasellus vehicula consequat auctor. Nunc ac erat in lorem venenatis bibendum. Morbi sed laoreet erat, ac maximus turpis. Sed lacinia purus turpis, non maximus velit mattis sed. Proin efficitur vehicula augue, sed suscipit tortor. In dolor ante, dictum in commodo et, volutpat id lectus. Suspendisse posuere elit id est venenatis efficitur. Nulla lorem nisi, dictum non enim sed, sagittis fringilla ex.
+        </p>
+        <p>
+          Vestibulum luctus consectetur magna ac viverra. Cras rutrum, urna vitae fermentum rhoncus, purus ipsum eleifend eros, et feugiat erat dui nec leo. Mauris vulputate erat nisi, a blandit justo mollis sed. Maecenas commodo varius ligula, vestibulum dignissim magna consequat eu. Nunc accumsan bibendum hendrerit. In aliquam lorem vitae sollicitudin euismod. Aliquam vitae congue sapien, in rhoncus massa. Quisque porttitor vestibulum erat eget congue.
+        </p>
+        <p>  
+          Aenean fringilla semper cursus. Integer sit amet interdum tortor. Nulla congue sem ac turpis mollis aliquet. Suspendisse et felis at sem consectetur tincidunt vitae ac elit. In maximus sed nisi quis aliquam. Sed velit augue, semper non turpis id, rhoncus bibendum dolor. Morbi nec malesuada dolor, in gravida nulla. Phasellus metus erat, dignissim nec urna ut, finibus consectetur nibh. Aliquam imperdiet est at augue facilisis, in porttitor purus maximus. Maecenas et tortor massa. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam in gravida elit. Vestibulum convallis lacinia erat, sit amet commodo felis ultrices vitae. Donec sem ipsum, venenatis eget est sit amet, imperdiet eleifend massa.
+          
+          <blockquote>
+            Proin consectetur nisi ullamcorper consectetur pharetra. Nullam in ipsum pretium, varius mauris eget, hendrerit tellus. Fusce gravida rhoncus dolor, sit amet elementum est ornare ut. Vestibulum at commodo lectus. Suspendisse lectus ligula, rutrum id leo quis, laoreet ornare ligula. In in tristique lorem. Praesent sed tortor sit amet dolor hendrerit hendrerit. Nam aliquet leo at lectus molestie condimentum. Phasellus a arcu varius, tempus dolor nec, pulvinar ipsum. Proin molestie urna lorem, vel semper diam dictum ut. Mauris commodo commodo tincidunt. Maecenas posuere malesuada risus et gravida. Morbi nec massa eu odio sagittis feugiat. Proin elit urna, tempor et eleifend id, hendrerit nec dolor.
+          </blockquote>
+          
+        </p>
+
+        <p>
+            Maecenas imperdiet malesuada nunc, id ornare justo. Proin dolor turpis, blandit ac posuere quis, bibendum et est. Integer dignissim sed ex tempor scelerisque. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec luctus ac diam quis interdum. Aenean a eros et nulla fringilla luctus. Suspendisse ac dui ac ex varius elementum at ultrices massa.              
+        </p>
+
+        <footer>
+
+        </footer>
+      </article>
+      <article>
+        <header>
+          <h1>Lorem Ipsum</h1>
+        </header>
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque convallis fringilla mi, quis mattis nisi facilisis vehicula. In vel tempus magna. Quisque vitae velit tellus. Fusce commodo urna sit amet lectus eleifend, sit amet pharetra tortor fermentum. Nullam fringilla turpis ipsum, ac pellentesque mi rutrum vitae. Etiam aliquet elit eu nunc mollis ullamcorper. Phasellus vehicula consequat auctor. Nunc ac erat in lorem venenatis bibendum. Morbi sed laoreet erat, ac maximus turpis. Sed lacinia purus turpis, non maximus velit mattis sed. Proin efficitur vehicula augue, sed suscipit tortor. In dolor ante, dictum in commodo et, volutpat id lectus. Suspendisse posuere elit id est venenatis efficitur. Nulla lorem nisi, dictum non enim sed, sagittis fringilla ex.
+        </p>
+        <p>
+          Vestibulum luctus consectetur magna ac viverra. Cras rutrum, urna vitae fermentum rhoncus, purus ipsum eleifend eros, et feugiat erat dui nec leo. Mauris vulputate erat nisi, a blandit justo mollis sed. Maecenas commodo varius ligula, vestibulum dignissim magna consequat eu. Nunc accumsan bibendum hendrerit. In aliquam lorem vitae sollicitudin euismod. Aliquam vitae congue sapien, in rhoncus massa. Quisque porttitor vestibulum erat eget congue.
+        </p>
+        <p>  
+          Aenean fringilla semper cursus. Integer sit amet interdum tortor. Nulla congue sem ac turpis mollis aliquet. Suspendisse et felis at sem consectetur tincidunt vitae ac elit. In maximus sed nisi quis aliquam. Sed velit augue, semper non turpis id, rhoncus bibendum dolor. Morbi nec malesuada dolor, in gravida nulla. Phasellus metus erat, dignissim nec urna ut, finibus consectetur nibh. Aliquam imperdiet est at augue facilisis, in porttitor purus maximus. Maecenas et tortor massa. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam in gravida elit. Vestibulum convallis lacinia erat, sit amet commodo felis ultrices vitae. Donec sem ipsum, venenatis eget est sit amet, imperdiet eleifend massa.
+          
+          <blockquote>
+            Proin consectetur nisi ullamcorper consectetur pharetra. Nullam in ipsum pretium, varius mauris eget, hendrerit tellus. Fusce gravida rhoncus dolor, sit amet elementum est ornare ut. Vestibulum at commodo lectus. Suspendisse lectus ligula, rutrum id leo quis, laoreet ornare ligula. In in tristique lorem. Praesent sed tortor sit amet dolor hendrerit hendrerit. Nam aliquet leo at lectus molestie condimentum. Phasellus a arcu varius, tempus dolor nec, pulvinar ipsum. Proin molestie urna lorem, vel semper diam dictum ut. Mauris commodo commodo tincidunt. Maecenas posuere malesuada risus et gravida. Morbi nec massa eu odio sagittis feugiat. Proin elit urna, tempor et eleifend id, hendrerit nec dolor.
+          </blockquote>
+          
+        </p>
+
+        <p>
+          Maecenas imperdiet malesuada nunc, id ornare justo. Proin dolor turpis, blandit ac posuere quis, bibendum et est. Integer dignissim sed ex tempor scelerisque. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec luctus ac diam quis interdum. Aenean a eros et nulla fringilla luctus. Suspendisse ac dui ac ex varius elementum at ultrices massa.              
+        </p>
+
+        <footer>
+
+        </footer>
+      </article>
+    </main>
+    <Footer />
+
+
+  </div>
 );
 
 ReactDOM.render(<App />, document.getElementById('app'));
